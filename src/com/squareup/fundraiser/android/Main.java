43fa9ecd8041402168360acb89db499a9d325237
Bob Lee <crazybob@crazybob.org>
2010-06-07 16:51:56 -0400

    More UI.

--- src/com/squareup/fundraiser/android/Main.java
+++ src/com/squareup/fundraiser/android/Main.java
@@ -3,6 +3,7 @@ package com.squareup.fundraiser.android;
 import android.app.Activity;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Collects the following information:
@@ -20,13 +21,42 @@ import android.os.Bundle;
  */
 public class Main extends Activity {
 
+  private TextView name;
+  private TextView email;
+  private TextView street;
+  private TextView city;
+  private TextView state;
+  private TextView zip;
+  private TextView employer;
+  private TextView occupation;
+  private TextView amount;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getWindow().setFormat(PixelFormat.RGBA_8888);
 
     setContentView(R.layout.main);
-    
-    findViewById(R.id.state).setNextFocusDownId(R.id.zip);
+
+    name = findTextViewById(R.id.name);
+    email = findTextViewById(R.id.email);
+    street = findTextViewById(R.id.street);
+    city = findTextViewById(R.id.city);
+    state = findTextViewById(R.id.state);
+    zip = findTextViewById(R.id.zip);
+    employer = findTextViewById(R.id.employer);
+    occupation = findTextViewById(R.id.occupation);
+    amount = findTextViewById(R.id.amount);
+
+    // Set up focus navigation around the zip field.
+    state.setNextFocusDownId(R.id.zip);
+    employer.setNextFocusUpId(R.id.zip);
+    zip.setNextFocusUpId(R.id.state);
   }
+
+  private TextView findTextViewById(int id) {
+    return (TextView) findViewById(id);
+  }
+
+  
 }
